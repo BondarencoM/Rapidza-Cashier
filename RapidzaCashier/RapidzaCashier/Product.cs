5cf68fdd3d1d4d7b40697b8cdd2d6ebe926fc2e6
Mihail Bondarenco <441044@student.fontys.nl>
2019-10-12 14:55:16 +0200

    feat(class architecture) add Product struct to represent one available Pizza

---
+++ RapidzaCashier/RapidzaCashier/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RapidzaCashier
+{
+    struct Product
+    {
+        public string Name { get; }
+        public string Image { get; }
+        public double Price { get; }
+
+        public Product(string name, string image, double price)
+        {
+            Name = name;
+            Image = image;
+            Price = price;
+        }
+        public Product(string name, double price)
+        {
+            throw new NotImplementedException("this constructor is not yet implemented");
+            Name = name;
+            Image = "Insert here path to default image";
+            Price = price;
+        }
+    }
+}
