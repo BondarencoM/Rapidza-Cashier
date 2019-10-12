6dc9f04be76dec79a282d430c4be955f4c439d48
Mihail Bondarenco <441044@student.fontys.nl>
2019-10-12 15:11:10 +0200

    feat(class architecture) add OrderProduct class representing an item in the order (product and count)

---
+++ RapidzaCashier/RapidzaCashier/OrderProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RapidzaCashier
+{
+    class OrderProduct
+    {
+        public Product product { get; }
+        public int count { get; set; }
+
+        public OrderProduct(Product product)
+        {
+            this.product = product;
+        }
+
+        public OrderProduct(Product product, int count) :this(product)
+        {
+            this.count = count;
+        }
+    }
+}
